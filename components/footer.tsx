322e4ba2c64c395108d3bdf8ec64ad7148c53dc3
Emmanuel Coll <131720993+Emonkeymc21@users.noreply.github.com>
2026-03-03 08:33:24 -0300

    Add files via upload

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,7 @@ export function Footer() {
   return (
     <footer className="mt-16 border-t border-border/60 py-10">
       <Container className="text-sm text-text/70">
-        <p>
         <p>Hecho para la comunidad rolera. Que tengas buenas tiradas 🎲</p>
-        </p>
-        </p>
       </Container>
     </footer>
   );
